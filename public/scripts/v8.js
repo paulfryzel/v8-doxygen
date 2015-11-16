f735f11cd86588a52a6127d5cab0226381d99641
Paul Fryzel <paul.fryzel@gmail.com>
2015-11-16 15:39:25 -0500

    Filter deprecation macros from nav-tree

--- public/scripts/v8.js
+++ public/scripts/v8.js
@@ -1,6 +1,6 @@
-function getIterator(filterMethod) {
+function getIterator(root, filterMethod) {
   return document.createNodeIterator(
-    document.body,
+    root,
     NodeFilter.SHOW_TEXT,
     {
       acceptNode: function(node) {
@@ -14,22 +14,33 @@ function getIterator(filterMethod) {
 }
 
 function tagDeprecated(root, filterMethod, className, ancestorSelector) {
-  var nodeIterator = getIterator(filterMethod);
+  var nodeIterator = getIterator(root, filterMethod);
   var node;
 
   while ((node = nodeIterator.nextNode())) {
     var closest = jQuery(node).closest(ancestorSelector);
     closest.addClass(className);
-    if (closest[0].nodeName === 'TR') {
-      var separator = closest.prev('tr[class^="separator"]');
-      separator.addClass(className);
+
+    var nodeName = closest[0].nodeName;
+    var container = null;
+
+    switch (nodeName) {
+    default:
+      break;
+    case 'TR':
+      container = closest.prev('tr[class^="separator"]');
+      container.addClass(className);
+      break;
+    case 'A':
+      container = closest.closest('li');
+      container.addClass(className);
+      break;
     }
   }
 }
 
 function filter(node) {
-  return node.data.indexOf('V8_DEPRECATE_SOON') > -1 &&
-         node.data.indexOf('V8_DEPRECATED');
+  return node.data.indexOf('V8_DEPRECATE') > -1;
 }
 
 function hideDeprecated() {
@@ -40,10 +51,31 @@ function init() {
   jQuery('#hide-deprecated').change(function(e) {
     hideDeprecated();
   });
+  jQuery('#nav-tree').click(function(e) {
+    tag(e.currentTarget, 'a[class^="classv8"]');
+  });
+}
+
+// Redefine dynsections::toggleInherit for hidden deprecated method support
+function toggleInherit(id) {
+  var rows = $('tr.inherit.'+id);
+  var img = $('tr.inherit_header.'+id+' img');
+  var src = $(img).attr('src');
+  if ($(img).attr('src').indexOf('open') > -1) {
+    rows.css('display','none');
+    $(img).attr('src',src.substring(0,src.length-8)+'closed.png');
+  } else {
+    rows.css('display','table-row'); // using show() causes jump in firefox
+    $(img).attr('src',src.substring(0,src.length-10)+'open.png');
+  }
+}
+
+function tag(root, selector) {
+  tagDeprecated(root, filter, 'deprecated', selector)
 }
 
 jQuery(document).ready(function() {
   init();
-  tagDeprecated(document.body, filter, 'deprecated', 'div.memitem,tr[class^="memitem"]')
+  tag(document.body, 'div.memitem,tr[class^="memitem"]');
   hideDeprecated();
 });
